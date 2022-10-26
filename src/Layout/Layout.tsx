27b06b5a9b48ebe2df552ca30bcb1100e8564057
Зенкин Владислав <zenych99@mail.ru>
2022-10-26 23:35:26 +0300

    setup base MiniMenu

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 import "./Layout.scss";
 
 const Layout = () => {
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
   return (
     <div className="Layout">
       <Header></Header>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Logo from "../Logo/Logo";
+import MiniMenu from "../MiniMenu/MiniMenu";
 import User from "../User/User";
 import "./Header.scss";
 const Header = () => {
   return (
     <div className="Header">
+      <MiniMenu></MiniMenu>
       <Logo></Logo>
       <User></User>
     </div>
